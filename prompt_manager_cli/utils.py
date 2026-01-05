7f619107e9261b31f418eb5a507df918927f697b
Sebastian Haglund <sebastian@rebase.energy>
2026-01-05 14:34:06 +0100

    update default template

--- prompt_manager_cli/utils.py
+++ prompt_manager_cli/utils.py
@@ -11,14 +11,20 @@ git_hash: "{git_hash}"
 cwd: "{cwd}"
 ---
 
+# Actor
+You are a senior software engineer.
+
 # Context and goals
 
 # Tools, APIs, docs and keys
 - This repo is using uv for package management and virtual environments [uv](https://docs.astral.sh/uv/). The virtual environment is named `.venv`.
 
 # Constraints and guidelines
+- The developed code should strive for high readability and maintainability.
 
 # Success metrics
+
+# Future intentions
 '''
 
 
